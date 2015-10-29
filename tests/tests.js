e56281ff217f6ac3ca31bf16960d8132648b87a2
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-10-29 15:15:19 +0100

    Añadidos tests con sinon

--- tests/tests.js
+++ tests/tests.js
@@ -8,6 +8,14 @@ describe("Test BDD para el Conversor de Temperatura", function() {
     var temp = new Temperatura();
     temp.set_valor(45e2);
     temp.set_tipo("F");
+
+    var callback = sinon.spy();
+    callback(temp.get_valor());
+
+    expect(callback.called).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.firstCall.calledWith(45e2)).to.be.true;
+    expect(callback.firstCall.calledWith(sinon.match.number)).to.be.true;
     expect(temp.get_valor()).to.equal(45e2);
     expect(temp.get_tipo()).to.equal("F");
   });
@@ -16,6 +24,14 @@ describe("Test BDD para el Conversor de Temperatura", function() {
     var temp = new Temperatura();
     temp.set_valor(-5e-2);
     temp.set_tipo("C");
+
+    var callback = sinon.spy();
+    callback(temp.get_tipo());
+
+    expect(callback.called).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.firstCall.calledWith("C")).to.be.true;
+    expect(callback.firstCall.calledWith(sinon.match.string)).to.be.true;
     expect(temp.get_valor()).to.equal(-5e-2);
     expect(temp.get_tipo()).to.equal("C");
   });
@@ -24,6 +40,14 @@ describe("Test BDD para el Conversor de Temperatura", function() {
     var temp = new Temperatura();
     temp.set_valor(10.1e10);
     temp.set_tipo("F");
+
+    var callback = sinon.spy();
+    callback(temp.get_tipo());
+
+    expect(callback.called).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.firstCall.calledWith("F")).to.be.true;
+    expect(callback.firstCall.calledWith(sinon.match.string)).to.be.true;
     expect(temp.get_valor()).to.equal(10.1e10);
     expect(temp.get_tipo()).to.equal("F");
   });
@@ -32,6 +56,15 @@ describe("Test BDD para el Conversor de Temperatura", function() {
     var temp = new Temperatura();
     temp.set_valor(12.35);
     temp.set_tipo("C");
+
+    var callback = sinon.spy();
+    callback(temp.get_valor());
+
+    expect(callback.called).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.firstCall.calledWith(12.35)).to.be.true;
+    expect(callback.firstCall.calledWith(sinon.match.number)).to.be.true;
+
     var result = temp.conversor();
     expect(result).to.equal("El resultado es: 54.230000000000004 F");
   });
@@ -40,6 +73,15 @@ describe("Test BDD para el Conversor de Temperatura", function() {
     var temp = new Temperatura();
     temp.set_valor(32);
     temp.set_tipo("F");
+
+    var callback = sinon.spy();
+    callback(temp.get_valor());
+
+    expect(callback.called).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.firstCall.calledWith(32)).to.be.true;
+    expect(callback.firstCall.calledWith(sinon.match.number)).to.be.true;
+
     var result = temp.conversor();
     expect(result).to.equal("El resultado es: 0 C");
   });
@@ -48,6 +90,12 @@ describe("Test BDD para el Conversor de Temperatura", function() {
      var temp = new Temperatura();
      temp.inicializar("0.032C");
 
+     var callback = sinon.stub();
+     callback.withArgs(0.032).returns(temp.get_valor());
+     callback.throws();
+
+     expect(callback(0.032)).to.equal(temp.get_valor());
+     expect(callback).to.throw(Error);
      expect(temp.get_valor()).to.equal(0.032);
      expect(temp.get_tipo()).to.equal("C");
    });
@@ -57,6 +105,12 @@ describe("Test BDD para el Conversor de Temperatura", function() {
       ini.value = "0.032C";
       conversor();
 
+      var callback = sinon.stub();
+      callback.withArgs("El resultado es: 32.0576 F").returns(conversor());
+      callback.throws();
+
+      expect(callback("El resultado es: 32.0576 F")).to.equal(conversor());
+      expect(callback).to.throw(Error);
       expect(resultado.innerHTML).to.equal("El resultado es: 32.0576 F");
     }
   });
